801227a9b7d41750de1fb656c30606b5ce4d2186
Dan Carper <dcarper@4moms.com>
2012-10-07 11:30:00 -0400

    typo

--- lib/suitable_services/railtie.rb
+++ lib/suitable_services/railtie.rb
@@ -1,6 +1,6 @@
 module SuitableServices 
   class Railtie < Rails::Railtie
-		intialize 'suitable_services.configure_rails_initialization' do
+		initialize 'suitable_services.configure_rails_initialization' do
 			Base.include ActionControllerMethods
 		end
 	end
