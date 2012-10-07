b0fc2d2abf660cad1582f8add648ac0474d3fe35
Dan Carper <dcarper@4moms.com>
2012-10-07 11:48:07 -0400

    send include to Base

--- lib/suitable_services/railtie.rb
+++ lib/suitable_services/railtie.rb
@@ -1,7 +1,7 @@
 module SuitableServices 
   class SuitableServicesRailtie < Rails::Railtie
 		initializer 'suitable_services_railtie.configure_rails_initialization' do
-			Base.include ActionControllerMethods
+			Base.send :include, ActionControllerMethods
 		end
 	end
 end
